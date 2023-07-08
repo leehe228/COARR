8252cc1a191427a2ba05dd82a7009c2adf926736
leehe228 <hoeunlee228@gmail.com>
2023-07-08 16:23:07 +0900

    Updated front

--- server/project/urls.py
+++ server/project/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('page.urls')),
-]
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
+] + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)

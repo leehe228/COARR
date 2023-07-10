eb01b7c632a781325ffbe81110fc647bfe49e689
Hoeun Lee <hoeunlee228@gmail.com>
2023-07-11 01:29:06 +0900

    Add review.html

--- test/main.js
+++ test/main.js
@@ -3,15 +3,15 @@ function to_follow() {
     let follow_page = document.getElementById('main-page-follow');
     let hline2 = document.getElementById('hline-right');
     let hline1 = document.getElementById('hline-left');
-    let htbutton1 = document.getElementById('header-text-b1');
-    let htbutton2 = document.getElementById('header-text-b2');
+    let htbutton1 = document.getElementById('header-button-h1');
+    let htbutton2 = document.getElementById('header-button-h2');
 
     recomm_page.setAttribute('style', 'display: none;');
     follow_page.setAttribute('style', 'margin-top:90px; margin-bottom:72px;');
     hline2.setAttribute('style', 'background: #000;');
     hline1.removeAttribute('style');
-    htbutton1.setAttribute('src', './websrc/follow-h1.png');
-    htbutton2.setAttribute('src', './websrc/follow-h2.png');
+    htbutton1.setAttribute('class', 'header-button-disable pointer');
+    htbutton2.setAttribute('class', 'header-button pointer');
 }
 
 function to_recomm() {
@@ -19,13 +19,13 @@ function to_recomm() {
     let follow_page = document.getElementById('main-page-follow');
     let hline1 = document.getElementById('hline-left');
     let hline2 = document.getElementById('hline-right');
-    let htbutton1 = document.getElementById('header-text-b1');
-    let htbutton2 = document.getElementById('header-text-b2');
+    let htbutton1 = document.getElementById('header-button-h1');
+    let htbutton2 = document.getElementById('header-button-h2');
 
     recomm_page.setAttribute('style', 'margin-top:90px; margin-bottom:72px;');
     follow_page.setAttribute('style', 'display: none;');
     hline1.setAttribute('style', 'background: #000;');
     hline2.removeAttribute('style');
-    htbutton1.setAttribute('src', './websrc/home-h1.png');
-    htbutton2.setAttribute('src', './websrc/home-h2.png');
+    htbutton1.setAttribute('class', 'header-button pointer');
+    htbutton2.setAttribute('class', 'header-button-disable pointer');
 }

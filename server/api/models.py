e264aff3d36ecce248d06b929a0a14819c89653a
leehe228 <hoeunlee228@gmail.com>
2023-07-11 05:36:32 +0900

    DB migrations

--- server/api/models.py
+++ server/api/models.py
@@ -7,6 +7,9 @@ class User(models.Model):
     EMAIL = models.CharField(max_length=100)
     NAME = models.CharField(max_length=10)
     PASSWORD = models.CharField(max_length=50)
+    LIKES = models.TextField(blank=True)
+    SAVED = models.TextField(blank=True)
+    FOLLOWING = models.TextField(blank=True)
     CREATED = models.DateTimeField(
             default=timezone.now)
     UPDATED = models.DateTimeField(
@@ -14,6 +17,17 @@ class User(models.Model):
 
     def __str__(self):
         return self.UID
+
+class Restaurant(models.Model):
+    RID = models.IntegerField()
+    STATUS = models.IntegerField(default=0)
+    NAME = models.CharField(max_length=30)
+    CONTENT = models.TextField(blank=True)
+    REVIEWS = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.RID
+    
 
 
 class Content(models.Model):
@@ -23,7 +37,7 @@ class Content(models.Model):
     APPROVER = models.IntegerField()
     DELETED = models.IntegerField()
     STATUS = models.IntegerField()
-    CONTENT = models.TextField()
+    CONTENT = models.TextField(blank=True)
     NUM_LIKE = models.IntegerField(default=0)
     NUM_SAVED = models.IntegerField(default=0)
     NUM_SHARE = models.IntegerField(default=0)
@@ -33,4 +47,4 @@ class Content(models.Model):
             default=timezone.now)
 
     def __str__(self):
-        return self.RID
+        return self.CID

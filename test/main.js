8f1a456a3c7219fc1ecd8a31af6c44494cc70f22
Hoeun Lee <hoeunlee228@gmail.com>
2023-07-14 14:51:04 +0900

    fetch test

--- test/main.js
+++ test/main.js
@@ -1,13 +1,5 @@
 let loading_page = document.getElementById('loading-page');
 
-// let title_input = document.getElementById('title-input');
-// let editor1 = document.getElementById('editor1');
-// let editor2 = document.getElementById('editor2');
-
-// title_input.tabIndex = 0;
-// editor1.tabIndex = 1;
-// editor2.tabIndex = 2;
-
 function to_follow() {
     let recomm_page = document.getElementById('main-page-recommend');
     let follow_page = document.getElementById('main-page-follow');
@@ -58,11 +50,11 @@ function cb_check(o) {
 function radio_check(n) {
     let r1 = document.getElementById('radio-lunch');
     let r2 = document.getElementById('radio-dinner');
-    
+
     if (n === 1) {
         r1.setAttribute('class', 'radio-button r-active');
         r2.setAttribute('class', 'radio-button');
-        
+
         r1.removeAttribute('style');
         r2.removeAttribute('style');
 
@@ -72,7 +64,7 @@ function radio_check(n) {
     else if (n === 2) {
         r1.setAttribute('class', 'radio-button');
         r2.setAttribute('class', 'radio-button r-active');
-    
+
         r1.setAttribute('style', 'border-right: 1px solid #A21E31;');
         r2.setAttribute('style', 'border-left: 1px solid #0000;');
 
@@ -93,7 +85,7 @@ function radio_check(n) {
 function stylebar(o) {
     let sb = document.getElementById('tool-style-btn');
     let style_bar = document.getElementById('editor-menubar');
-    
+
     if (o === true) {
         // open
         style_bar.setAttribute('style', 'border-top: #0000;');
@@ -108,7 +100,7 @@ function stylebar(o) {
 function upload_review() {
     // checkbox
     let cb = document.getElementById('review-cb');
-    
+
     // lunch dinner radio button
     let r1 = document.getElementById('radio-lunch');
     let r2 = document.getElementById('radio-dinner');
@@ -117,18 +109,19 @@ function upload_review() {
     let title_input = document.getElementById('title-input');
     let editor1 = document.getElementById('editor1');
     let editor2 = document.getElementById('editor2');
-    
+
     // 식당
     let restaurant_input = document.getElementById('restaurant-name-text');
-    
-    // 방문일자 
+
+    // 방문일자
     let visit_date_input = document.getElementById('visit-date-text');
 
-    console.log(title_input.value.trim());
-    console.log(editor1.textContent.trim());
-    console.log(editor2.textContent.trim());
-    
-    if (title_input.value.trim() === '' || editor1.textContent.trim() === '' || editor2.textContent.trim() === '') {
+    // test
+    title_input.textContent = "테스트 데이터 제목";
+    editor1.textContent = "테스트 데이터 내용1";
+    editor2.textContent = "테스트 데이터 내용2";
+
+    if (title_input.textContent.trim() === '' || editor1.textContent.trim() === '' || editor2.textContent.trim() === '') {
         alert('내용을 모두 입력해주세요.');
         return;
     }
@@ -137,14 +130,14 @@ function upload_review() {
         alert('식사 시간을 선택해주세요.');
         return;
     }
-    
+
     if (cb.getAttribute('checked') === 'false') {
         alert('어뷰징 리뷰 및 허위 광고가 아님을 확인해주세요');
         return;
     }
 
     //loading_page.removeAttribute('style');
-    
+
     // upload to server
     var jlist = new Array();
 
@@ -156,7 +149,7 @@ function upload_review() {
     data.RID = 0;
     data.visit_date = Date.now();
     jlist.push(data);
-    
+
     upload_data(JSON.stringify(jlist));
 }
 

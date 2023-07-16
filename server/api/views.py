c03a03176ca6b0a7c9b1b16750a06569c39a7989
leehe228 <hoeunlee228@gmail.com>
2023-07-16 13:03:45 +0000

    Update api views

--- server/api/views.py
+++ server/api/views.py
@@ -153,16 +153,17 @@ def review_gpt(data, jobject, CID):
     except Exception as e:
         print(e)
 
-        eval_json = {
-            "criterion1":{
-                "ratings":0,
-                "evalutation":"점수를 평가할 수 없습니다."
-            },
-            "criterion2":{
-                "ratings":0,
-                "evaluation":"점수를 평가할 수 없습니다."
-            }
-        }
+        #eval_json = {
+        #    "criterion1":{
+        #        "ratings":0,
+        #        "evalutation":"점수를 평가할 수 없습니다."
+        #    },
+        #    "criterion2":{
+        #        "ratings":0,
+        #        "evaluation":"점수를 평가할 수 없습니다."
+        #    }
+        #}
+        eval_json = json.loads(eval_en)
     
     save_gpt_review(eval_json, jobject, CID)
 
